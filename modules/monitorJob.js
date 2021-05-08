5a15b142e0a554c28778e66c173337eb6b7483cf
Chris Palka <cpalka87gmail.com>
2021-05-07 22:03:45 -0600

    Resolve handling no response for monitorJob from   monitorScraper   -Add QT url for balko bot via webhook

--- modules/monitorJob.js
+++ modules/monitorJob.js
@@ -12,6 +12,7 @@ export default setInterval(async () => {
         const handle = productArray[productArray.length - 1];
         const domain = productArray[2];
         const { user_id, product } = item;
+        const productLink = item.product;
         axios.post('/getMonitorWebhook', {
           user_id,
         })
@@ -22,23 +23,30 @@ export default setInterval(async () => {
             return monitorScraper(data.data, handle);
           })
           .then((response) => {
-            const result = [];
-            const delimitedResult = [];
-            title = response.title;
-            response.variants.forEach((product) => {
-              result.push(product.id);
-              delimitedResult.push(
-                `${product.title.replace(/[^0-9.]+/g, '')} - ${product.id}`
-              )
-            })
-            productImage = response.featured_image !== undefined ? `http:${response.featured_image}` : notFound
-            if (response.available) {
-              const message = (`\`\`\`${result.join('\n')}\`\`\``);
-              const delimitedMessage = (`\`\`\`${delimitedResult.join('\n')}\`\`\``);
-              webhookMessage(domain, webhookURL, product, message, `RESTOCK -- ${title}`, productImage);
-              webhookMessage(domain, webhookURL, product, delimitedMessage, `RESTOCK -- ${title}`, productImage);
+            if (response) {
+              const QT = `[Balko](http://localhost:6776/?url=${productLink})`
+              const result = [];
+              const delimitedResult = [];
+              title = response.title;
+              response.variants.forEach((product) => {
+                if (product.available) {
+                  result.push(product.id);
+                  delimitedResult.push(
+                    `${product.title.replace(/[^0-9.]+/g, '')} - ${product.id}`
+                  )
+                }
+              })
+              productImage = response.featured_image !== undefined ? `http:${response.featured_image}` : notFound
+              if (response.available) {
+                const message = (`\`\`\`${result.join('\n')}\`\`\``);
+                const delimitedMessage = (`\`\`\`${delimitedResult.join('\n')}\`\`\``);
+                webhookMessage(domain, webhookURL, product, QT, message, `RESTOCK -- ${title}`, productImage);
+                webhookMessage(domain, webhookURL, product, QT, delimitedMessage, `RESTOCK -- ${title}`, productImage);
+              } else {
+                return false;
+              }
             } else {
-              return false;
+              return false
             }
           })
           .catch((err) => console.log(err));
--- modules/webhookMonitor.js
+++ modules/webhookMonitor.js
@@ -1,11 +1,12 @@
 const webhook = require('webhook-discord');
 
-const webhookMessage = (domain, webhookURL, productLink, message, title, image) => {
+const webhookMessage = (domain, webhookURL, productLink, QT, message, title, image) => {
   const Hook = new webhook.Webhook(webhookURL);
   const msg = new webhook.MessageBuilder()
     .setName('DonkWizard')
     .setThumbnail(image)
     .addField(title, productLink, false)
+    .addField('QT', QT)
     .addField('Variants', message, true)
     .addField('Site', domain, true)
     .setColor('#4071bf');
