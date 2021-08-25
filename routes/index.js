80b46875ed0085d39e5e80b15671e0a07dd28ae3
Chris Palka <cpalka87gmail.com>
2021-08-24 20:03:17 -0600

    Remove clubMonitor

--- routes/index.js
+++ routes/index.js
@@ -35,8 +35,12 @@ dotenv.config({ path: path.join(__dirname, '../.env') });
 
 const router = Router();
 
+const headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'}
+
 router.get('/clubMonitor', async (req, res) => {
-  axios('https://www.costco.com/callaway-edge-10-piece-golf-club-set%2C-right-handed---stiff-flex.product.100683880.html')
+  axios('https://www.costco.com/callaway-edge-10-piece-golf-club-set%2C-right-handed---stiff-flex.product.100683880.html', {
+    headers: headers,
+  })
     .then((response) => {
       res.status(200).send('Available')
         // const $ = cheerio.load(response.data, { xmlMode: false });
@@ -50,7 +54,9 @@ router.get('/clubMonitor', async (req, res) => {
     .catch((err) => res.status(404).send('Product Not Found'));
 });
 router.get('/clubMonitorTwo', async (req, res) => {
-  axios('https://www.costco.com/.product.1477082.html')
+  axios('https://www.costco.com/.product.1477082.html', {
+    headers: headers,
+  })
     .then((response) => {
       res.status(200).send('Available')
         // const $ = cheerio.load(response.data, { xmlMode: false });
